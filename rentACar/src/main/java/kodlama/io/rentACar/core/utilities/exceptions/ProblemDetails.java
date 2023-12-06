f97f9bc5223ad51ba62a0067b4d21612405a4a59
aislyndev <aaislyndev@gmail.com>
2023-12-06 14:44:52 +0300

    merhaba

---
+++ rentACar/src/main/java/kodlama/io/rentACar/core/utilities/exceptions/ProblemDetails.java
@@ -0,0 +1,18 @@
+package kodlama.io.rentACar.core.utilities.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProblemDetails {
+	String message;
+
+	public void setMesage(String message2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ rentACar/src/main/java/kodlama/io/rentACar/core/utilities/exceptions/ValidationProblemDetails.java
@@ -0,0 +1,12 @@
+package kodlama.io.rentACar.core.utilities.exceptions;
+
+
+public class ValidationProblemDetails extends ProblemDetails {
+
+
+	public void setMesage(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
